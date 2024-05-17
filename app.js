4164c5e837cf1de28d78341c26314d32d839ffcd
ayed87 <gake12234@gmail.com>
2024-05-17 17:51:07 +0300

    fix core option

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(cors());
 
 // CORS configuration
 const corsOptions = {
-	origin: "https://rofof-api-production.up.railway.app", // Set this to your frontend's URL
+	origin: "*", // Set this to your frontend's URL
 	credentials: true, // This allows the backend to accept cookies from the frontend
 };
 
